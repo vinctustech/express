f8e30787b298194c4f9baff989938808c6050085
Edward A Maxedon, Sr <edadma@vinctus.com>
2020-12-10 18:07:24 -0500

    #1 request handlers can return a promise

--- src/main/scala/com/vinctus/express/express.scala
+++ src/main/scala/com/vinctus/express/express.scala
@@ -1,16 +1,12 @@
 package com.vinctus
 
-//import typings.express.mod.{RequestHandler => TypingsRequestHandler}
-//import typings.expressServeStaticCore.mod.{ParamsDictionary, Query => TypingsQuery}
-
 import scala.scalajs.js
 import scala.scalajs.js.|
 
 package object express {
-//  type TRequestHandler = TypingsRequestHandler[ParamsDictionary, _, _, TypingsQuery]
   type Next = js.Function1[js.Any, Unit]
   type Query = typings.qs.mod.ParsedQs
-  type RequestHandler = js.Function3[Request, Response, js.Dynamic, Unit]
-  type ErrorHandler[E <: js.Object] = js.Function4[E, Request, Response, js.Dynamic, Unit]
+  type RequestHandler = js.Function3[Request, Response, js.Dynamic, Any]
+  type ErrorHandler[E <: js.Object] = js.Function4[E, Request, Response, js.Dynamic, Any]
   type PathParams = String | js.RegExp | Array[String | js.RegExp]
 }
